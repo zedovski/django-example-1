a465e2942bc23d7b84bfa08e66070996549aa8c0
Reza Ravari <rravari@Leviathan17.local>
2017-12-10 18:37:05 +0800

    first commit

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from basic_app import views
+
+# FOR TEMPALTE TAGGING
+app_name = 'basic_app'
+
+urlpatterns = [
+
+    url(r'^relative/$', views.relative, name='relative'),
+    url(r'^other/$', views.other, name='other'),
+
+]
